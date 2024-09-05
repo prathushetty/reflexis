5d4251a4e9be786d8d8b4f2d8ddfcdfa0d7df3eb
Prathvin R Shetty <116250867+prathushetty@users.noreply.github.com>
2024-09-05 22:09:21 +0530

    Update db2archlogs_cleanup.sh

--- db2archlogs_cleanup.sh
+++ db2archlogs_cleanup.sh
@@ -4,44 +4,77 @@
 ### Usage: sh db2archlogs_cleanup.sh <retention_days>
 #################################################################################
 
-# Set retention period to 4 days if not provided
-if [ -z "$1" ]; then
-    Retention=4
+# # Set retention period to 4 days if not provided
+# if [ -z "$1" ]; then
+#     Retention=4
+# else
+#     Retention=$1
+# fi
+
+# # Get hostname and date
+# HOST_NAME=$(hostname | cut -d"." -f1)
+# FILE_DATE=$(date +%Y-%m-%d)
+
+# # Set up a base directory in the home directory for testing
+# BASEDIR=~/test_db2archlogs_cleanup
+# LOGGER=${BASEDIR}/${HOST_NAME}_${FILE_DATE}.log
+
+# # Create base directory if it doesn't exist
+# if [ ! -d "$BASEDIR" ]; then
+#     mkdir -p $BASEDIR
+# fi
+
+# # Create a test log file to simulate the operation
+# echo "Creating test files for cleanup..." >> $LOGGER
+# touch $BASEDIR/testfile1.LOG
+# touch $BASEDIR/testfile2.LOG.gz
+
+# # Log the start of the cleanup process
+# echo "Purging files older than ${Retention} days" >> $LOGGER
+# echo "START purging: $(date)" >> $LOGGER
+
+# # Simulate the cleanup operation
+# find $BASEDIR -type f -name "*.LOG" -mtime +$Retention -exec rm -v {} \; >> $LOGGER
+# find $BASEDIR -type f -name "*.LOG.gz" -mtime +$Retention -exec rm -v {} \; >> $LOGGER
+
+# # Simulate cleanup of old log files in the same directory
+# find $BASEDIR -type f -name "$HOST_NAME*.log" -mtime +2 -exec rm -v {} \; >> $LOGGER
+
+# # Log the end of the cleanup process
+# echo "END purging: $(date)" >> $LOGGER
+
+# # Output the log for review
+# cat $LOGGER
+
+
+#/bin/bash
+#################################################################################
+### Script to clean up the /db2archlogs directory based on retentaion period
+### passed as value.
+### Usage sh db2archlogs_cleanup.sh value1
+### Author: Prateek Kante, Reflexis Systems Inc.
+#################################################################################
+
+if [ -z "$1" ];then
+Retention=4
 else
-    Retention=$1
+Retention=$1
 fi
-
-# Get hostname and date
-HOST_NAME=$(hostname | cut -d"." -f1)
-FILE_DATE=$(date +%Y-%m-%d)
-
-# Set up a base directory in the home directory for testing
-BASEDIR=~/test_db2archlogs_cleanup
+HOST_NAME=`hostname|cut -d"." -f1`
+FILE_DATE=`date +%Y-%m-%d`
+BASEDIR=/test_db2archlogs_cleanup
 LOGGER=${BASEDIR}/${HOST_NAME}_${FILE_DATE}.log
 
-# Create base directory if it doesn't exist
 if [ ! -d "$BASEDIR" ]; then
     mkdir -p $BASEDIR
 fi
+echo "Purging less than ${Retention} days">>$LOGGER
+echo "START purging :`date`">>$LOGGER
+########################## db2archlogs Retention ###################################
+find /db2archlogs1 -type f -name "S*.LOG" -mtime +$Retention -exec rm -v {} \; >>$LOGGER
+find /db2archlogs1 -type f -name "S*.LOG".gz -mtime +$Retention -exec rm -v {} \; >>$LOGGER
+find /dbbackups/logs/scripts/db2archlogs_cleanup -type f -name "$HOST_NAME*.log" -mtime +2 -exec rm -v {} \; >>$LOGGER
+echo "END purging:`date`">>$LOGGER
 
-# Create a test log file to simulate the operation
-echo "Creating test files for cleanup..." >> $LOGGER
-touch $BASEDIR/testfile1.LOG
-touch $BASEDIR/testfile2.LOG.gz
 
-# Log the start of the cleanup process
-echo "Purging files older than ${Retention} days" >> $LOGGER
-echo "START purging: $(date)" >> $LOGGER
 
-# Simulate the cleanup operation
-find $BASEDIR -type f -name "*.LOG" -mtime +$Retention -exec rm -v {} \; >> $LOGGER
-find $BASEDIR -type f -name "*.LOG.gz" -mtime +$Retention -exec rm -v {} \; >> $LOGGER
-
-# Simulate cleanup of old log files in the same directory
-find $BASEDIR -type f -name "$HOST_NAME*.log" -mtime +2 -exec rm -v {} \; >> $LOGGER
-
-# Log the end of the cleanup process
-echo "END purging: $(date)" >> $LOGGER
-
-# Output the log for review
-cat $LOGGER
